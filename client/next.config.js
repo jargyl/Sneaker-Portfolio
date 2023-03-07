d2e9bebdd6e9cda4aee20a4d55c87db5e0d3067c
jargyl <jari.gielen@gmail.com>
2023-03-07 13:32:02 +0100

    Added config

--- client/next.config.js
+++ client/next.config.js
@@ -7,6 +7,17 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
   },
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      "/": { page: "/" },
+      "/edit": { page: "/edit" },
+      "/admin": { page: "/admin" },
+      "/404": { page: "/404" },
+    };
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
